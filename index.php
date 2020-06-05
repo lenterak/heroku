0e48eee8234cbed0d63245fd5d30693107e28f4b
lenterak <weblenterakecil@yahoo.com>
2020-06-05 23:30:34 +0700

    ganti gsc

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
 <title>Kanal Pengetahuan dan Informasi umum</title>
 <meta name="description" content="blog yang membahas tentang pengetahuan informasi menarik di sekitar kita dari beberapa sumber" />
-<meta name="google-site-verification" content="iIThTrE6U3Hi1d8V_juH_ZROI8JDjyZgQwH1RfrNxyc" />
+<meta name="google-site-verification" content="DMD09sKaTTZmKMvqMpJSS1dvFIS5ZCdCFtTe4ECkCMY" />
 <style type='text/css'>
 html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline;}
 article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block;}body{line-height:1;display:block;}*{margin:0;padding:0;}html{display:block;}ol,ul{list-style:none;}blockquote,q{quotes:none;}blockquote:before,blockquote:after,q:before,q:after{background:transparent;}table{border-collapse:collapse;border-spacing:0;}
